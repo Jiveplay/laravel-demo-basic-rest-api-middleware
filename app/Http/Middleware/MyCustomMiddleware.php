12ba6b281b12588f65c84e01f43d1977da8d5136
John Binzak <john.binzak@gmail.com>
2020-04-21 15:32:35 -0500

    ###Set Data in Middleware Before we link our middleware to a controller, let's give it some basic data to pass through. We will give the request 3 new data fields. One from a `URL param`, the next from a `Http Header`, and the last being a custom timestamp. We will be able to access this data in our controller. [SNIPPET]

--- app/Http/Middleware/MyCustomMiddleware.php
+++ app/Http/Middleware/MyCustomMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-// [SNIPPET]
 class MyCustomMiddleware{
 
     /**
@@ -14,11 +13,22 @@ class MyCustomMiddleware{
      * @param  \Closure  $next
      * @return mixed
      */
+
+    // [SNIPPET]
     public function handle($request, Closure $next){
+
+        // set from url param
+        $request->test_key = $request->has('test_key') ? $request->input('test_key') : 'foo';
+
+        // set from header
+        $request->my_header = $request->header('x-custom-header');
+
+        // set constant
+        $request->blah = date("Y-m-d H:i:s");
 
         // continue
         return $next($request);
     }
+    // [/SNIPPET]
 
 }
-// [/SNIPPET]
