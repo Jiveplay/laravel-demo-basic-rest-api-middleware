bff73dc123e5feed8a18a1b9f9c646f28c234044
John Binzak <john.binzak@gmail.com>
2020-04-21 15:37:46 -0500

    ###Apply Middleware to Controller Now let's apply the middleware to a specific controller. We will do so in the `constructor` of the controller and by using the `name shortcode` we specified while registering the middleware with our app. The middleware will now be apply to all endpoints in this controller. Within a given endpoint we can now access the data we created in `MyCustomMiddleware.php`. [SNIPPET]

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// [SNIPPET]
+class TestController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware('my-custom-middleware');
+    }
+
+    public function test_abc(Request $request){
+
+        // check if data field is set and non-empty, if so let's use it, otherwise use the default
+        $data = [
+            "test_key" => isset($request->test_key) && !empty($request->test_key) ? $request->test_key : 'NO TEST KEY',
+            "my_header" => isset($request->my_header) && !empty($request->my_header) ? $request->my_header : 'NO HEADER',
+            "blah" => isset($request->blah) && !empty($request->blah) ? $request->blah : 'NO BLAH'
+        ];
+
+        // response
+        return response()->json($data, 200);
+    }
+
+}
+// [/SNIPPET]
